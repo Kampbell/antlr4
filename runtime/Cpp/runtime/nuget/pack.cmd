e6417a233448d81912508dd302ef2c3ef3090c7a
Francis ANDRE <francis.andre.kampbell@orange.fr>
2021-01-11 20:46:46 +0100

    Add VSTOOLS setup
    Signed-off-by: Francis ANDRE <francis.andre.kampbell@orange.fr>

--- runtime/Cpp/runtime/nuget/pack.cmd
+++ runtime/Cpp/runtime/nuget/pack.cmd
@@ -4,8 +4,71 @@ rem echo ------
 rem echo pack [vsvers] [version]       // pack 2019 4.9.1
 rem echo ------
 setlocal enableextensions enabledelayedexpansion
+
 if "%1"=="" goto usage
 if "%2"=="" goto usage
+
+set PLATFORM=Win32
+
+rem -version ^^[16.0^^,17.0^^)
+set VS_VERSION=vs%1
+rem  should be set "VSWHERE='%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe  -property installationPath -version ^[16.0^,17.0^)'"
+if %VS_VERSION%==vs2019 (
+  set "VSWHERE='C:\PROGRA~2\"Microsoft Visual Studio"\Installer\vswhere.exe  -latest -property installationPath -version ^[16.0^,17.0^)'"
+) else (
+if %VS_VERSION%==vs2017 (
+  set "VSWHERE='C:\PROGRA~2\"Microsoft Visual Studio"\Installer\vswhere.exe  -latest -property installationPath -version ^[15.0^,16.0^)'"
+)
+)
+for /f " delims=" %%a in (%VSWHERE%) do @set "VSCOMNTOOLS=%%a"
+
+echo ============= %VSCOMNTOOLS% =============
+
+if %VS_VERSION%==vs2019 (
+  set VS_VARSALL=..\..\VC\Auxiliary\Build\vcvarsall.bat
+  set "VS160COMNTOOLS=%VSCOMNTOOLS%\Common7\Tools\"
+) else (
+  if %VS_VERSION%==vs2017 (
+    set VS_VARSALL=..\..\VC\Auxiliary\Build\vcvarsall.bat
+    set "VS150COMNTOOLS=%VSCOMNTOOLS%\Common7\Tools\"
+  ) else (
+    set VS_VARSALL=..\..\VC\vcvarsall.bat
+  )
+)
+
+if not defined VCINSTALLDIR (
+  if %VS_VERSION%==vs2015 (
+    if %PLATFORM%==x64 (
+      call "%VS140COMNTOOLS%%VS_VARSALL%" x86_amd64 8.1
+    ) else (
+      call "%VS140COMNTOOLS%%VS_VARSALL%" x86 8.1
+    )
+  ) else (
+    if %VS_VERSION%==vs2017 (
+      if %PLATFORM%==x64 (
+        call "%VS150COMNTOOLS%%VS_VARSALL%" x86_amd64 8.1
+      ) else (
+        call "%VS150COMNTOOLS%%VS_VARSALL%" x86 8.1
+      )
+    ) else (
+      if %VS_VERSION%==vs2019 (
+        if %PLATFORM%==x64 (
+          call "%VS160COMNTOOLS%%VS_VARSALL%" x86_amd64 8.1
+        ) else (
+          call "%VS160COMNTOOLS%%VS_VARSALL%" x86 8.1
+        )
+      )
+    )
+  )
+)
+
+if not defined VSINSTALLDIR (
+  echo Error: No Visual C++ environment found.
+  echo Please run this script from a Visual Studio Command Prompt
+  echo or run "%%VSnnCOMNTOOLS%%\vsvars32.bat" first.
+  goto :buildfailed
+)
+
 
 pushd ..\
 call msbuild antlr4cpp-vs%1.vcxproj -t:rebuild -p:Configuration="Debug DLL"
@@ -26,5 +89,6 @@ echo ------
 echo "pack [vsvers] [version]"       // pack 2019 4.9.1
 echo ------
 :exit
+:buildfailed
 endlocal
 rem echo on
